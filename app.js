fc742a18e6263cfc54c2a7d0358fdc4e03411ee7
spahicadis <adis@spahica.com>
2024-09-11 13:14:23 +0200

    Next steps fix logic for same query and add some css for responsive layout

--- app.js
+++ app.js
@@ -2,23 +2,69 @@
 const searchBox = document.getElementById('search-box');
 const searchBtn = document.querySelector('.search-btn');
 const rootRenderingDiv = document.querySelector('.rendering-photos');
+const showMore = document.querySelector('.button-css');
 
 
 //STATES
 const ID = '2Hg3VfHcbx2p8Whb_c8P4MKGw_ocZxVPYKo6kcvIixM';
 let querySearch;
+let numberOfPage = 1;
 
 //BINDING
 
 searchBox.addEventListener('input', ((e) => {
 querySearch = e.target.value;
-console.log(querySearch)
+console.log()
+
 
 }))
 
-searchBtn.addEventListener('click', () => {
+let copyOfSearch = querySearch
 
-  fetch(`https://api.unsplash.com/search/photos?query=${querySearch}`, {
+function fetchRender() {
+  fetch(`https://api.unsplash.com/search/photos?page=${numberOfPage}&query=${querySearch}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Client-ID 2Hg3VfHcbx2p8Whb_c8P4MKGw_ocZxVPYKo6kcvIixM`
+    }
+    }).then((results) => results.json())
+      .then((data) => {
+        console.log(data.results)
+        console.log(data.results.length)
+        for(let i = 0; i<=data.results.length; i++) {
+          const divs = document.createElement('div')
+          const imgs = document.createElement('img');
+          const ps = document.createElement('p');
+          imgs.setAttribute('src', `${data.results[i].urls.small_s3}`)
+          imgs.classList.add('grid-items')
+          ps.innerHTML = `${data.results[i].alt_description}`;
+          divs.classList.add('divs');
+          ps.classList.add('ps');
+          divs.appendChild(imgs)
+          divs.appendChild(ps)
+          rootRenderingDiv.appendChild(divs)
+          showMore.style.display = 'inline';
+          
+          
+        }
+      });
+      if(querySearch !== copyOfSearch) {
+        copyOfSearch=querySearch
+        rootRenderingDiv.innerHTML = '';
+      }
+
+    
+}
+
+searchBtn.addEventListener('click', fetchRender);
+
+
+
+
+
+showMore.addEventListener('click', (() => {
+  ++numberOfPage
+  fetch(`https://api.unsplash.com/search/photos?page=${numberOfPage}&query=${querySearch}`, {
     method: 'GET',
     headers: {
       Authorization: `Client-ID 2Hg3VfHcbx2p8Whb_c8P4MKGw_ocZxVPYKo6kcvIixM`
@@ -39,11 +85,12 @@ searchBtn.addEventListener('click', () => {
           divs.appendChild(imgs)
           divs.appendChild(ps)
           rootRenderingDiv.appendChild(divs)
+          showMore.style.display = 'inline';
+          
+          
         }
       });
-
-})
-
+}) )
 
 
 
@@ -59,7 +106,9 @@ searchBtn.addEventListener('click', () => {
 
 
 
-//.then((data) => console.log(data.results.length))
+
+
+
 
 
 
